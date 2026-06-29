5411f092d4b6b543299c74581b29db44769419bf
Josh Edmonson <jtedmons@asu.edu>
2026-06-29 11:00:55 -0400

    Add clear exception assignment endpoint and tests

--- tests/ForecastExceptionPortal.Api.Tests/ExceptionEndpointTests.cs
+++ tests/ForecastExceptionPortal.Api.Tests/ExceptionEndpointTests.cs
@@ -3,6 +3,8 @@ namespace ForecastExceptionPortal.Api.Tests;
 using System.Net;
 using Microsoft.AspNetCore.Mvc.Testing;
 using Xunit;
+using System.Net.Http.Json;
+using ForecastExceptionPortal.Api.Models;
 
 public class ExceptionEndpointTests
 {
@@ -49,5 +51,31 @@ public class ExceptionEndpointTests
         using var client = application.CreateClient();
         var response = await client.GetAsync("/api/exceptions?status=Investigating&severity=Critical");
         Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+    }
+
+    [Fact]
+    public async Task ClearExceptionAssignment_WithExistingId_ReturnsOk()
+    {
+        await using var application = new WebApplicationFactory<Program>();
+        using var client = application.CreateClient();
+
+        var response = await client.DeleteAsync("/api/exceptions/2/assignment");
+
+        Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+
+        var updatedExceptionRecord = await response.Content.ReadFromJsonAsync<ExceptionRecord>();
+        Assert.NotNull(updatedExceptionRecord);
+        Assert.Null(updatedExceptionRecord.AssignedTo);
+    }
+
+    [Fact]
+    public async Task ClearExceptionAssignment_WithMissingId_ReturnsNotFound()
+    {
+        await using var application = new WebApplicationFactory<Program>();
+        using var client = application.CreateClient();
+
+        var response = await client.DeleteAsync("/api/exceptions/999/assignment");
+
+        Assert.Equal(HttpStatusCode.NotFound, response.StatusCode);
     }
 }
